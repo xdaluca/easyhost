d29cd4b1fc2cb12a328b312f3239a1aec5e27053
xdaluca <xdaluca@pm.me>
2023-04-10 18:45:20 -0300

    add user model to backend

---
+++ backend/models/user.model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
