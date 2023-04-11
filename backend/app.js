05188da8715a3eea15c513094834563e3bca9b58
xdaluca <xdaluca@pm.me>
2023-04-11 08:43:53 -0300

    web3js backend configuration

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,29 @@ const mongoose = require('mongoose');
 const propertyRoutes = require('./routes/property.routes');
 const bookingRoutes = require('./routes/booking.routes');
 const User = require('./models/user.model');
+const path = require('path');
+
+// Web3 configuration
+const Web3 = require('web3');
+const web3 = new Web3('http://127.0.0.1:8545'); // Replace with your Ethereum network URL
+
+const bookingABI = require(path.join(__dirname, '..', 'build', 'contracts', 'Booking.json')).abi; // Replace with the path to your Booking.json
+const bookingAddress = '0x1FDa306e7eAdee82564328B58286cC2CA2d4bD13'; // Replace with your contract address
+const bookingContract = new web3.eth.Contract(bookingABI, bookingAddress);
+
+async function createBookingOnChain(propertyId, checkInDate, checkOutDate, totalAmount, userAddress) {
+    const gasEstimate = await bookingContract.methods
+      .createBooking(propertyId, checkInDate, checkOutDate, totalAmount)
+      .estimateGas({ from: userAddress });
+  
+    const result = await bookingContract.methods
+      .createBooking(propertyId, checkInDate, checkOutDate, totalAmount)
+      .send({ from: userAddress, gas: gasEstimate });
+  
+    return result;
+  }
+// Add this line after the createBookingOnChain function definition
+exports.createBookingOnChain = createBookingOnChain;
 
 const app = express();
 
