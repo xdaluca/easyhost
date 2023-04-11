3080671594f067615aaa03d91fc290b55ede4f61
xdaluca <xdaluca@pm.me>
2023-04-11 18:03:36 -0300

    simple front-end for testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Web3 from 'web3';
+import axios from 'axios';
 
 function App() {
+  const [property, setProperty] = useState(0); 
+  const [checkInDate, setCheckInDate] = useState('');
+  const [checkOutDate, setCheckOutDate] = useState('');
+  const [totalAmount, setTotalAmount] = useState('');
+
+  const handleBooking = async (e) => {
+    e.preventDefault();
+  
+    // Connect to MetaMask and get the user's Ethereum address
+    const web3 = new Web3(Web3.givenProvider);
+    const ethereum = window.ethereum;
+  
+    if (ethereum) {
+      try {
+        // Request account access from the user
+        await ethereum.request({ method: 'eth_requestAccounts' });
+        const accounts = await web3.eth.getAccounts();
+        const user = accounts[0];
+  
+        // Send the booking data to your back-end
+        try {
+          const response = await axios.post('http://localhost:5001/api/bookings', {
+            property: parseInt(property),
+            user,
+            checkInDate,
+            checkOutDate,
+            totalAmount,
+          });
+  
+          console.log(response.data);
+          alert('Booking successful!');
+        } catch (error) {
+          console.error(error);
+          alert('Error creating booking');
+        }
+      } catch (error) {
+        console.error(error);
+        alert('Error connecting to MetaMask');
+      }
+    } else {
+      alert('MetaMask is not installed');
+    }
+  };  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Create a booking</h1>
+      <form onSubmit={handleBooking}>
+        <label>
+          Property ID:
+          <input type="number" value={property} onChange={(e) => setProperty(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Check-in date:
+          <input type="date" value={checkInDate} onChange={(e) => setCheckInDate(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Check-out date:
+          <input type="date" value={checkOutDate} onChange={(e) => setCheckOutDate(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Total amount:
+          <input type="number" value={totalAmount} onChange={(e) => setTotalAmount(e.target.value)} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
